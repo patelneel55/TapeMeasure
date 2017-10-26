74a73ae28eaca2a539191e58c5782093d14b8af5
alpallion <alpallion@yahoo.com>
2017-10-25 22:17:32 -0500

    Inital Commit

--- app/src/main/java/com/tapemeasure/MainActivity.java
+++ app/src/main/java/com/tapemeasure/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+
+/*Its Yea Boi*/
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
